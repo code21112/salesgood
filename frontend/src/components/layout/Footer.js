996439c86675a52be2d573ad5a344473867c102a
code21112 <code21112@protonmail.com>
2021-03-18 19:42:39 -0400

    Modifying text in Footer component Footer.js

--- frontend/src/components/layout/Footer.js
+++ frontend/src/components/layout/Footer.js
@@ -5,7 +5,7 @@ const Footer = () => {
         <Fragment>
             <footer className="py-1">
                 <p className="text-center mt-1">
-                    Sales Good - 2021 Copyright, All Rights Reserved
+                    Sales Good - 2021 Copyright. All Rights Reserved @code21112
                 </p>
             </footer>
         </Fragment>
